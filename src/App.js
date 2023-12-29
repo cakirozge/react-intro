f6ce2da5854d17756e1a9c7d725be21ce3402e15
Ozge Cakir <caakir.ozge@gmail.com>
2023-12-29 12:38:44 +0300

    events & useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+
+//JSX => JavaScript + Html : JavaScript Xml açılımı
 function App() {
+  //değiştiğinde UI'i da update edecek değişken
+  //useState: react fonksiyon (react hooks-- jsx güncellemesi için)
+
+  //let totalCount =0;
+  const [total, setTotal] = useState(0); //{get; set;}
+
+
+  const increase = () => {
+    setTotal(total + 1);
+
+  }
+
+  const decrease = () => {
+   setTotal(total - 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   
+   <p>Toplam Tıklama Sayısı: {total}</p>
+   <button onClick={()=> {
+    increase();
+    console.log("+ butonuna basıldı.")
+   }}>+</button>
+   <button onClick={()=>{decrease();}}>-</button> 
+
+   
+   </>
   );
 }
+// export const variable = 1;
+// export const variable2 = 1;
+//  <button onClick={()=>{decrease();}}>-</button> 
 
+// EVENT - useState kullanımındaki syntax.
+//  <button onClick={decrease}>-</button>  -- referans veriri.
+// <button onClick={()=>{decrease();}}>-</button> --fonksiyonu cağırırım.
 export default App;
+
+
