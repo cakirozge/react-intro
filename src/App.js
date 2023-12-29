90da4e17fad127790c536d2116d920ec6101c9e9
Ozge Cakir <caakir.ozge@gmail.com>
2023-12-29 13:33:43 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 
@@ -7,19 +7,30 @@ import './App.css';
 function App() {
   //değiştiğinde UI'i da update edecek değişken
   //useState: react fonksiyon (react hooks-- jsx güncellemesi için)
+  //useEffect (fonksiyon, [dep_list])
 
   //let totalCount =0;
   const [total, setTotal] = useState(0); //{get; set;}
 
 
-  const increase = () => {
-    setTotal(total + 1);
-
+  const increase = () =>{
+    setTotal((prevState)=>{ return prevState  + 1; }); //async işlem (colsole çıktı:0 bu nedenle.)
+    console.log(total);
   }
 
   const decrease = () => {
-   setTotal(total - 1);
+   setTotal(total - 1); //async
+   
   }
+  //dep listedeki değişkenlerden herhangi biri değiştiğinde tetiklenir.
+  //her sayfa acılışı use effect'i tetikler.
+  
+  useEffect(()=> {
+    console.log("use effect calıştı..")
+    console.log(total);
+  }, [total]);
+
+
   return (
    <>
    
@@ -41,6 +52,11 @@ function App() {
 // EVENT - useState kullanımındaki syntax.
 //  <button onClick={decrease}>-</button>  -- referans veriri.
 // <button onClick={()=>{decrease();}}>-</button> --fonksiyonu cağırırım.
+
+//useState:
+//-useState'in setterı async işlemdir. Bu nedenle kodda 0 çıktısı aldık.
+//-async işlem: O satırın işlemini beklemeyen, kodu bloke etmeyen yapılardır. console.log(totali okuyor ilk işlem bitmeden)
+//- tüm set; işemleri async'dır.
 export default App;
 
 
