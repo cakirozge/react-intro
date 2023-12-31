039dd2b1bf6db988bb3a90c0f3cfd89a22ae977c
Ozge Cakir <caakir.ozge@gmail.com>
2023-12-31 12:08:40 +0300

    bootsrap

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -4,13 +4,10 @@ import { Link } from 'react-router-dom'
 export default function Homepage() {
   return (
     <div>
-        Homepage
+        <button className="btn btn-primary">Homepage</button>
         <br /> 
         {/* SPA'larda href kullanımı yanlıştır. */}
-        <a href="/about">About'a git.</a>
-        <br />
         <Link to={"/about"} >About'a git.</Link>
-        
     </div>
   )
 }
