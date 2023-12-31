5676d33646d8f16b58556bebb2b9180f07aefeb2
Ozge Cakir <caakir.ozge@gmail.com>
2023-12-31 12:38:48 +0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { BrowserRouter,Route, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/Homepage/Homepage';
 import About from './pages/About/About';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar />
    <BrowserRouter>
    <Routes>
     <Route path="/" element={<Homepage />}></Route>
@@ -12,6 +15,8 @@ function App() {
     <Route path="*" element={<p>Not Found</p>}></Route>
    </Routes>
    </BrowserRouter>
+   <footer />
+   </>
   );
 }
 
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Homepage() {
   return (
     <div>
-        <button className="btn btn-primary">Homepage</button>
+        <button className="btn btn-danger">Homepage</button>
         <br /> 
         {/* SPA'larda href kullanımı yanlıştır. */}
         <Link to={"/about"} >About'a git.</Link>
