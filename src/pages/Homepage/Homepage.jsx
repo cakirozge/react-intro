79af91b41cab042d445fa8b81d70f1db32d57812
Ozge Cakir <caakir.ozge@gmail.com>
2023-12-31 18:01:26 +0300

    uı create

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,79 +1,39 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../../components/ProductCard/ProductCard';
+import axios from 'axios';
+
 
 export default function Homepage() {
-  const myAsyncFunction = () => {
-    return new Promise((resolve, reject) => {
-      reject("HTTP VERİSİ");
-    });
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    fetchProducts();
+  },[]);
+
+  const fetchProducts = async ()=>{
+    let response = await axios.get('https://dummyjson.com/products');
+    setProducts(response.data.products);
+    setProducts(response.data.products);
+
   };
 
-  useEffect(() => {
-    makeAsyncCall();
-    makeHttpCall();
-  }, []);
 
-  const makeHttpCall = async () => {
-    // fetch("https://dummyjson.com/products")
-    // //zincirleme promise yapısı var.
-    //   .then((response) => response.json())
-    //   .then(json => console.log(json))
-    //   .catch((err) => console.log(err));
 
-    //---Alternatif syntax
-    //2 tane then kullanıldığı için 2 tane let var.
-    let response = await fetch("https://dummyjson.com/products");
-    let json = await response.json();
-    console.log(json);
- 
-  };
-
-  const makeAsyncCall = async () => {
-    //makeAsyncCall();
-    // .then((response) => {
-    //         console.log("İşlem başarılı.", response);
-    //       }) //async işlem başarılı ise (resolve edildiyse)
-    //       .catch((error) => {
-    //         console.log("işlem başarısız.", error);
-    //       }) //async işlem başarısız ise (reject edildiyse)
-    //       .finally(() => {
-    //         console.log("işlem bitti.");
-
-    //es2017 async-await
-
-    //await keyword: promıse'in dönüş tipini response esitleriz.
-
-    //----Alternatif syntax
-    //try-catch-finall yerine await ile kullanıyorsam try-catch manuel yapmalıyım (reject olduğunda hata fırlar bu nedenle try catch bloğu kullanmalıyım.)
-
-    try {
-      let response = await myAsyncFunction();
-      console.log(response);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  // useEffect(() => {
-  //   myAsyncFunction()
-  //     .then((response) => {
-  //       console.log("İşlem başarılı.", response);
-  //     }) //async işlem başarılı ise (resolve edildiyse)
-  //     .catch((error) => {
-  //       console.log("işlem başarısız.", error);
-  //     }) //async işlem başarısız ise (reject edildiyse)
-  //     .finally(() => {
-  //       console.log("işlem bitti.");
-  //     }); //her halükarda en son çalışır.
-  //   console.log();
-  // }, []);
-
-  return (
-    <div>
-      <button className="btn btn-danger">Homepage</button>
-      <br />
-      {/* SPA'larda href kullanımı yanlıştır. */}
-      <Link to={"/about"}>About'a git.</Link>
-    </div>
+  return(
+      <div className='container mt-5'>
+        <div className='row'>
+          {products.map(
+            product => <div key={product.id}className='col-lg-3 col-md-6 col-12 mb-5'>
+            <ProductCard product={product} />
+            </div>)}
+        </div>
+      </div>
   );
 }
+
+
+//col-md-3 col-12 mb-5 ile 768px ve üzeri için 4den 1 düşürür.
+    
+  
+
+  
